e96abbf0d168d9605dcb0bb3dc3a833bdf5b282c
Aashna <aashnasuman19@gmail.com>
2026-05-01 06:59:48 +0530

    working MainDashboard and MainApp setup

---
+++ src/com/inventory/MainApp.java
@@ -0,0 +1,9 @@
+package com.inventory;
+
+import com.inventory.ui.MainDashboard;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new MainDashboard();
+    }
+}
--- src/com/inventory/ui/MainDashboard.java
+++ src/com/inventory/ui/MainDashboard.java
@@ -1,1 +1,32 @@
-package com.inventory.model;
+
+package com.inventory.ui;
+import javax.swing.*;
+
+//creating class
+public class MainDashboard extends JFrame {
+    //constructur
+    public MainDashboard(){
+        //window settings
+        setTitle("Inventory Management System");
+        setSize(800,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); //centre the window
+
+        //tabbed pane
+        JTabbedPane tabs = new JTabbedPane();
+
+        //add tabs empty for now
+        tabs.add("Products", new JPanel());
+        tabs.add("Stock", new JPanel());
+        tabs.add("Suppliers", new JPanel());
+        tabs.add("Invoice", new JPanel());
+        tabs.add("Reports", new JPanel());
+
+        //add tabs to frame
+        add(tabs);
+
+        //make visible
+        setVisible(true);
+    }
+    
+}
