5fdb82614f4fbc5bc2674db50c54a84dfa8b6097
Aashna <aashnasuman19@gmail.com>
2026-05-03 22:31:50 +0530

    final model, working properly

--- src/com/inventory/MainApp.java
+++ src/com/inventory/MainApp.java
@@ -3,21 +3,13 @@ package com.inventory;
 import com.inventory.ui.MainDashboard;
 import com.inventory.db.DBConnection;
 
-
 public class MainApp {
-
     public static void main(String[] args) {
 
         System.out.println("App starting...");
 
-        try {
-            DBConnection.getConnection();
-            System.out.println("Database connected successfully!");
-        } catch (Exception e) {
-            System.out.println("Database connection failed!");
-            e.printStackTrace();
-        }
+        DBConnection.getConnection();
 
-        new MainDashboard();
+        new MainDashboard(); // ✔ fixed
     }
 }
